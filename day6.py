1fd6e8dfa6bdae4f1e52361e5fcd676fa22f91d4
Himanshu <himanshusinghbisht71@gmail.com>
2026-07-30 17:59:57 +0530

    added -v inifinity question

--- day6.py
+++ day6.py
@@ -54,7 +54,74 @@ answer = sol.sortedSquares(nums)
 print(answer)
 
             
-      
+# Q2 Complete the find_max function. It takes as input a list of integers, nums, and returns a number.
+
+# max_so_far is initialized as negative infinity.
+# Compare each number in nums to max_so_far. If any number is larger than max_so_far, replace max_so_far with that value.
+# After iterating over every number, return max_so_far. If nums is empty, return negative infinity.
+
+def find_max(nums):
+    if nums == []:
+        return float("-inf")
+
+    max_so_far = float("-inf")
+    for num in nums:
+        if num > max_so_far:
+            max_so_far = num
+    return max_so_far
+        
+
+
+run_cases = [([1, 2, 3, 4, 5], 5), ([1, 2, 300, 4, 5], 300)]
+
+submit_cases = run_cases + [
+    ([1, 20, 3, 4, 5], 20),
+    ([-1, 2, 3, 4, 5], 5),
+    ([1, 2, 3, 21, 18], 21),
+    ([], float("-inf")),
+    ([-1, -2, -3, -4, -5], -1),
+]
+
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}")
+    result = find_max(input1)
+    print(f"Expected: {expected_output}")
+    print(f"Actual:   {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
+
         
         
 
