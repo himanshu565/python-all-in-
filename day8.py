a22c335a97501ce0b66463b727f78e6d05df1732
Himanshu <himanshusinghbisht71@gmail.com>
2026-08-03 13:35:44 +0530

    rage function reverse day 8

--- day8.py
+++ day8.py
@@ -5,6 +5,25 @@ dog = ("Fido",)
 # Because Tuples hold their data, multiple tuples can be stored within a list. Similar to storing other data in lists, each tuple within the list is separated by a comma. When accessing a list of tuples, the first index selects which tuple you want to access, the second selects a value within that tuple.
 
 
+def get_heroes():
+    heroes = [
+        ("Glorfindel",
+        2093,
+        True) ,
+        ("Gandalf",
+        1054,
+        False),
+        ("Gimli",
+        389,
+        False),
+        ("Aragorn",
+        87,
+        False),
+    ]
+
+    return heroes
+
+
 run_cases = [
     (
         [
@@ -28,6 +47,8 @@ run_cases = [
                 87,
                 False,
             ),
+            
+            
         ]
     ),
 ]
@@ -65,6 +86,74 @@ def main():
     skipped = len(submit_cases) - len(test_cases)
     for test_case in test_cases:
         correct = test(test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
+
+# Q2 reversing using range function 
+
+def reverse_list(items):
+    reverse = []
+    for i in range(len(items)-1,-1,-1):
+        reverse.append(items[i])
+    return reverse
+
+
+run_cases = [
+    (
+        ["Shortsword", "Healing Potion", "Iron Breastplate", "Kite Shield"],
+        ["Kite Shield", "Iron Breastplate", "Healing Potion", "Shortsword"],
+    ),
+    ([1, 2, 300, 4, 5], [5, 4, 300, 2, 1]),
+]
+
+submit_cases = run_cases + [
+    ([], []),
+    (["a"], ["a"]),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]),
+    (
+        ["apple", "banana", "cherry", "date", "elderberry"],
+        ["elderberry", "date", "cherry", "banana", "apple"],
+    ),
+    (["hello", "world"], ["world", "hello"]),
+]
+
+
+def test(input, expected_output):
+    print("---------------------------------")
+    print(f"Input list: {input}")
+    print(f"Expected reversed list: {expected_output}")
+    result = reverse_list(input)
+    print(f"Actual reversed list: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
         if correct:
             passed += 1
         else:
