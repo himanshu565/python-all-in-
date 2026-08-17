dbb57b4c7a9c2785387d94be662ab98459ecbb97
Himanshu <himanshusinghbisht71@gmail.com>
2026-08-17 18:01:41 +0530

    min max and filter using IN

--- SQL/day3.sql
+++ SQL/day3.sql
@@ -15,3 +15,20 @@ WHERE height = (
     SELECT max(height)
     FROM patients
   )
+
+-- Q3 Show all columns for patients who have one of the following patient_ids:
+-- 1,45,534,879,1000
+SELECT *
+FROM patients
+WHERE
+  patient_id IN (1, 45, 534, 879, 1000);
+
+-- Q4 Show the total number of admissions
+SELECT count(patient_id) as total_patients from admissions
+
+-- Q5 Show the patient id and the total number of admissions for patient_id 579.
+SELECT
+  patient_id,
+  COUNT(*) AS total_admissions
+FROM admissions
+WHERE patient_id = 579;
