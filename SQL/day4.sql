161764fac4b7abc1b66e0275d376a05a2b48c7d3
Himanshu <himanshusinghbisht71@gmail.com>
2026-08-28 21:08:28 +0530

    sql day 4  medium questions

--- SQL/day4.sql
+++ SQL/day4.sql
@@ -14,3 +14,31 @@ SELECT
   DISTINCT YEAR(birth_date) AS birth_year
 FROM patients
 ORDER BY birth_year;
+-- //Yes, GROUP BY produces a result set with distinct rows based on the columns specified in the grouping clause. 
+
+SELECT year(birth_date)
+FROM patients
+GROUP BY year(birth_date)
+
+-- Show first name, last name, and gender of patients whose gender is 'M'
+SELECT
+  first_name,
+  last_name,
+  gender
+FROM patients
+WHERE gender = 'M';
+
+
+-- Q4 how unique first names from the patients table which only occurs once in the list.
+SELECT first_name
+FROM patients
+GROUP BY first_name
+HAVING COUNT(first_name) = 1
+
+-- Q5 Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+SELECT
+  patient_id,
+  first_name
+FROM patients
+WHERE first_name LIKE 's____%s';
+/*
--- day12.py
+++ day12.py
@@ -74,3 +74,4 @@ if "__RUN__" in globals():
     test_cases = run_cases
 
 main()
+ 
